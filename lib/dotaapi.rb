541598bd8596fbe79bb9c4d42db39f46a618695d
tmharber <tommharber@gmail.com>
2015-10-08 16:05:00 +0100

    Updates

--- lib/dotaapi.rb
+++ lib/dotaapi.rb
@@ -1,6 +1,6 @@
 class Dotaapi
   def get_match_details(id)
-    response = Unirest.get("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=2D19DDD9C1E05C6EA66E26ADE925D1D1&match_id=#{id}")
+    response = Unirest.get("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=[redacted]&match_id=#{id}")
     Match.new(response.body["result"])
   end
 end
